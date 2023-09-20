73f11e9c1ddbd2da4d6d12013e317b02d99851f6
Carlwilliam <jdcarlwilliam@gmail.com>
2023-09-20 09:27:04 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const currentUser = 'Grace Hopper';
 
-const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const welcomeMessage = 'Welcome to Flatbook, ';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
